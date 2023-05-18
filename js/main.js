051d7d0f7fe77ff28cc47105937f5555b0203243
knightch <amankumarpandey2397@gmail.com>
2023-05-18 22:59:40 +0530

    initial setup

---
+++ js/main.js
@@ -0,0 +1,51 @@
+function revealToSpan() {
+    document.querySelectorAll(".reveal").forEach(
+        (elem) => {
+            let spanParent = document.createElement("span");
+            let spanChild = document.createElement("span");
+
+            spanParent.classList.add("parent");
+            spanChild.classList.add("child");
+
+            spanChild.innerHTML = elem.innerHTML;
+            spanParent.appendChild(spanChild);
+
+            elem.innerHTML = ""
+            elem.appendChild(spanParent)
+        }
+    )
+}
+revealToSpan();
+
+var tl = gsap.timeline();
+
+tl.from(".child span", {
+        x: "100px",
+        delay: 1,
+        stagger: .2,
+        duration: 1.4,
+        ease: Power3.easeInOut
+    })
+    .to(".parent .child", {
+        y: "-100%",
+        duration: 1,
+        ease: Circ.easeInOut
+    })
+    .to("#loader", {
+        height: 0,
+        duration: 1,
+        ease: Circ.easeInOut
+    })
+    .to("#green", {
+        height: "100%",
+        top: 0,
+        duration: 1,
+        delay: -1,
+        ease: Circ.easeInOut
+    })
+    .to("#green", {
+        height: "0%",
+        duration: 1,
+        delay: 1,
+        ease: Circ.easeInOut
+    })
